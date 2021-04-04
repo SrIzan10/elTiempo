37bddd7723ab126e5c75cf207d1d1cc721875f25
Alberto <garrierik@gmail.com>
2021-04-04 22:47:23 +0200

    Add files via upload

---
+++ js/date.js
@@ -0,0 +1,7 @@
+const fecha = new Date();
+const dato = fecha.getFullYear();
+
+document.getElementById('fecha').innerHTML= `
+
+<p  >Proyecto de <a class="enlace" target="_blank" href="https://twitter.com/gentrux">garri</a> 🔥 ${dato}</p>
+`;
