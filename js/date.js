61d27b4472e17ea3cc106bb86e0274db5230b724
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2021-08-03 14:03:55 +0200

    Update date.js

--- js/date.js
+++ js/date.js
@@ -3,5 +3,5 @@ const dato = fecha.getFullYear();
 
 document.getElementById('fecha').innerHTML= `
 
-<p  >Proyecto de <a class="enlace" target="_blank" href="https://twitter.com/gentrux">garri</a> 🔥 ${dato}</p>
+<p>Fork de <a href="github.srizan.ml">Sr Izan</a> ${dato}</p>
 `;
